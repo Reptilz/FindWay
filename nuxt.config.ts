5df196e3b2031cf4d030147e622517095845421e
Reptilz <jmorlet94@gmail.com>
2025-05-31 18:10:11 +0200

    Configuration project

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    'nuxt-icon',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@sidebase/nuxt-auth'
+  ],
+  // Configuration pour Pinia
+  pinia: {
+    autoImports: ['defineStore', 'acceptHMRUpdate'],
+  },
+  // Configuration pour TailwindCSS
+  tailwindcss: {
+    // Options de configuration si nécessaire
+  },
+  // Configuration pour Content
+  content: {
+    // Options de configuration pour le module content
+  },
+  // Configuration pour Image
+  image: {
+    // Options de configuration pour le module image
+  }
+})
